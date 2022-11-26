807f2df16b1ba6a1ec559b206a24de75173b4848
Ruannino <ruanfariasdc@hotmail.com>
2022-11-26 05:29:17 -0300

    Commit 25.11.2022

---
+++ aula3/comentarios.js
@@ -0,0 +1,15 @@
+// Escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// comentário
+
+/*
+Um bloco de comentário
+Linha comentário
+*/
+
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+// Vai ser ignorado
---
+++ aula3/index.js
@@ -0,0 +1,11 @@
+console.log('Ruannino');  // String = Texto
+console.log("Ruannino");
+console.log("'Ruannino'");
+console.log('"Ruannino"');
+console.log(`Ruannino`);
+console.log(`"Ruannino" 'Lucas'`);
+
+console.log(35, 15.85); // Number = Número
+
+console.log(`Meu nome é "Ruannino". 
+    Estou aprendendo JavaScript às`, 22, `da noite.`)
