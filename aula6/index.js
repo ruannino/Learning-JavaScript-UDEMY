926471537810ae85c7de77f8761a2e167e3fcc53
Ruannino <ruanfariasdc@hotmail.com>
2022-11-28 23:40:58 -0300

    28.11.2022

---
+++ aula6/index.js
@@ -0,0 +1,20 @@
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não pode conter espaçoes ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilizr VAR, utilize LET
+
+let nomeCliente = 'Luiz';
+let nomecliente = 'Otávio';
+console.log(nomeCliente, nomecliente);
+
+/*
+let nome; // Declarou a variável
+nome = 'Qualque valor'; // Inicializando a variável
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
+*/
