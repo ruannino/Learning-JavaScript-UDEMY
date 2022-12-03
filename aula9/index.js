587f8564a708d07ca9664822409ac677a93921a4
Ruannino <ruanfariasdc@hotmail.com>
2022-12-02 23:09:18 -0300

    02.12.2022

---
+++ aula9/index.js
@@ -0,0 +1,22 @@
+// ECMAScript 2015 (ES6) JavaScript
+// String, Number, Undefined, Null, Boolean, Symbol
+
+var nome = 'Luiz' // NÃO FAÇA ISSO!
+var nome = 'Otávio';
+
+console.log(nome);
+
+const nome1 = 'Luiz';
+const nome2 = "Luiz";
+const nome3 = `Luiz`;
+const num1 = 10;
+const num2 = 10.52;
+let nomeAluno; // undefined = não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo = não aponta pra local nenhum na memória
+const aprovado = true; // Boolean = true, false (lógico)
+
+console.log(typeof num1, num1);
+console.log(typeof num2, num2);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
