1ae9c8a24b9ab3aef93fcedcc62d1d407c46e890
ruannino <ruanfariasdc@hotmail.com>
2022-12-14 08:47:46 -0300

    commit 14.12.2022

---
+++ aula14/index.js
@@ -0,0 +1,12 @@
+// IEEE 7554-2008 -> Padrão de referência do JavaScript
+let num1 = 10;  // number
+let num2 = 2.5; // number
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+// console.log(num1.toString(2)); // versão binária
+// console.log(num1.toFixed(2));  // alterar número de casa decimais
+// console.log(Number.isInteger(num1));  // verifica se o número inteiro
+// let temp = num1 * 'ola';
+// console.log(temp);
+// console.log(Number.isNaN(temp)); // verifica se é NaN
