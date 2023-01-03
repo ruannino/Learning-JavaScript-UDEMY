f9e363a9333229a6597109bd2be78acf672ec10c
ruannino <ruanfariasdc@hotmail.com>
2023-01-02 22:17:58 -0300

    Commit 02.01.2023

---
+++ aula15/index.js
@@ -0,0 +1,17 @@
+let num1 = 9;
+
+// let num2 = Math.floor(num1); -> arredonda o numero para cima
+// let num2 = Math.ceil(num1); -> arredonda o numero para baixo
+// let num2 = Math.round(num1); -> arredonda o numero para o mais próximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); -> Maior número dessa sequência
+//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); -> Menor número dessa sequência
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(aleatorio);
+
+//console.log(Math.pow(2, 10));
+//console.log(2 ** 10);
+
+//console.log(num1 ** (1/2));  -> Raiz quadrada
+//console.log(num1 ** 0.5);  -> Raiz quadrada
+
+//console.log(100 / 0); -> número Infinity
