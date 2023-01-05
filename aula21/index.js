d17dc970a30cd2a72cccb863a217e53a46269a10
ruannino <ruanfariasdc@hotmail.com>
2023-01-04 22:09:15 -0300

    commit 04.11.23

---
+++ aula21/index.js
@@ -0,0 +1,17 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igua a 
+== igualdade (valor) **
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **
+!== diferente estrito (valo e tipo)
+*/
+const num1 = 10;
+const num2 = '10';
+const comp = num1 === num2;
+const compDif = num1 !== num2;
+console.log(comp);
+console.log(compDif);
---
+++ aula22/index.js
@@ -0,0 +1,11 @@
+/*
+Operadores Lógico
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> Não
+*/
+
+const usuario = 'Ruannino';
+const senha = '123456';
+const vaiLogar = usuario === 'Ruannino' && senha === '123456';
+console.log(vaiLogar);
